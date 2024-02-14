e59ee8e9e7ab465cf672ed59930e3b3829c6fb86
DevOpsinfogen <infogen.devops@gmail.com>
2024-02-14 17:08:26 +0530

    change app contant

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is changed code.......
+          Welcome to build 1 
+          keep smile 
         </p>
         <a
           className="App-link"
